322434babe025f3c1a80990e745514b4163a7a54
Eduardo Alencar <eduardocna@gmail.com>
2026-09-16 18:37:01 -0300

    fix: support GitHub Pages preview paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://eduardocnalencar.com',
+  base: '/eduardo-alencar-site',
   output: 'static',
   build: { format: 'directory' }
 });
